9bd4a44bebd9723723af5f17753bed2e526f619c
Kevin Sinatria <kevinsinatria8@gmail.com>
2025-05-11 18:06:16 +0700

    Finalizing RESTful API

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,8 +3,10 @@ const Boom = require("@hapi/boom");
 
 const authMiddleware = async (request, h) => {
   try {
-    const token = request.headers.authorization;
+    let token = request.headers.authorization;
     if (!token) throw Boom.unauthorized("Token tidak ditemukan");
+
+    token = token.replace("Bearer ", "");
 
     const decoded = Jwt.verify(token, process.env.JWT_SECRET);
     request.auth = decoded;
